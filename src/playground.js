f999cb0aadd1f0fdf17df148262cf666c0fa1a03
Ahmed Nagi <ahmedflnagi@gmail.com>
2023-01-20 15:38:58 +0200

    install tailwind]

---
+++ src/playground.js
@@ -0,0 +1,7 @@
+const fruits = ['apple', 'banana', 'cherry'];
+const vegetables = ['carrot', 'potato', 'tomato'];
+// merge the two arrays
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
+ 
